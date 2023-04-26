40c2fb267ef03a880050b87ca122bfadeea3c6f3
Brian Reid <31985319+brianreidc7@users.noreply.github.com>
2023-04-26 17:11:25 +0100

    Add better support for email address policies
    Added two columns to table to report more useful info

--- Start-ExchangeRecipientAdminCenter.ps1
+++ Start-ExchangeRecipientAdminCenter.ps1
@@ -196,13 +196,15 @@ try {
 
 				# Prepare email address policies list
 				$HTMLROWS = ""
-				foreach ($Item in (Get-EmailAddressPolicy | Select Name, Priority, RecipientFilter)) {
+				foreach ($Item in (Get-EmailAddressPolicy | Select Name, Priority, RecipientFilter,EnabledPrimarySMTPAddressTemplate,EnabledEmailAddressTemplates)) {
 					$HTMLROWS += "
 					<tr>
 					<th scope=`"row`">
 					<a href=`"#`">$($Item.Name)</a></th>
 					<td>$($Item.Priority)</td>
 					<td>$($Item.RecipientFilter)</td>
+					<td>$($Item.EnabledPrimarySMTPAddressTemplate)</td>
+					<td>$($Item.EnabledEmailAddressTemplates)</td>
 					</tr>";
 				}
 
